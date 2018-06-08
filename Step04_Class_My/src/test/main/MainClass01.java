ceeec58d2a8e5d0106ffdab1882458444ba6f405
shong <iohong21@naver.com>
2018-06-08 17:52:42 +0900

    Step04 Added

---
+++ Step04_Class_My/src/test/main/MainClass01.java
@@ -0,0 +1,21 @@
+package test.main;
+
+import test.mypac.MyUtil;
+/*
+ * [ 클래스의 용도 ]
+ * 
+ * 1. static 멤버를 포함하는 기능
+ * 2. 객체의 설계도 역활
+ * 3. data type 역활 
+ */
+public class MainClass01 {
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		// MyUtil 클래스의 version 이라는 필드에 있는 값을
+		// 참조해서 a 라는 이름의 지역 변수에 담기
+		int a = MyUtil.version;
+		
+		// MyUtil 클래스의 clean() 메소드 호출하기
+		MyUtil.clean();
+	}
+}
---
+++ Step04_Class_My/src/test/mypac/AttackUnit.java
@@ -0,0 +1,12 @@
+package test.mypac;
+
+public class AttackUnit {
+	// non static 멤버 필드
+	public int energy = 100;
+	
+	// non static 멤버 메소드
+	public void attack() {
+		System.out.println("공격해요!");
+	}
+	
+}

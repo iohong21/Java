69a5ad84bcbed546e2114bf35bf84521a40f66a2
shong <iohong21@naver.com>
2018-06-11 17:52:07 +0900

    Step06

---
+++ Step06_Operator_My/src/test/main/MainClass06.java
@@ -0,0 +1,24 @@
+package test.main;
+
+/*
+ * [ 연산자 ]
+ * 
+ * 6. 3항 연산자
+ * 
+ * - 형식
+ * boolean type data ? 값1 : 값2
+ */
+public class MainClass06 {
+	public static void main(String[] args) {
+		boolean isWait = true;
+		
+		String result = isWait ? "기다려" : "기다리지마";
+		
+		// 3 항 연산을 if~else 문으로 대체하면
+		if(isWait) {
+			result = "기다려";
+		} else {
+			result = "기다리지마";
+		}
+	}
+}
